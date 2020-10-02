2a4bdc1b88bb35ba31bf17ac975eb429afd05be7
Timofey <tima929_v@rambler.ru>
2020-10-02 23:26:27 +0300

    test commit

--- app/src/main/java/com/example/metronom/MainActivity3.java
+++ app/src/main/java/com/example/metronom/MainActivity3.java
@@ -91,7 +91,6 @@ public class MainActivity3 extends AppCompatActivity implements View.OnClickList
                         }
                     }
                 }
-
             }
         });
 
