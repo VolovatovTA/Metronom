143d847439e542e636c14b8187ca0d38c58db07f
Timofey <tima929_v@rambler.ru>
2020-10-12 17:50:04 +0300

    items

--- app/src/main/java/com/example/metronom/Track.java
+++ app/src/main/java/com/example/metronom/Track.java
@@ -4,13 +4,15 @@ public class Track {
     String name;
     int temp;
     int position;
+    int id;
     boolean box;
 
-    Track(String _name, int _temp, int _position, boolean _box) {
+    Track(String _name, int _temp, int _position, boolean _box, int _id) {
         name = _name;
         temp = _temp;
         position = _position;
         box = _box;
+        id = _id;
     }
 
 }
